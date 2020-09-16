73eb404fa3b2066d64580885ab4f55e7a359a5ac
GioVentura <55453857+GioVentura@users.noreply.github.com>
2020-09-16 11:59:07 +0200

    Add files via upload

---
+++ style/script.js
@@ -0,0 +1,12 @@
+/*-----------------------------------------------------------------------------------*/
+/*	01. MouseOver Landing Home
+/*-----------------------------------------------------------------------------------*/
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
