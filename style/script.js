9d0e255b2956a7c83e06f5eec76b7fafb0e0e758
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2020-11-20 13:36:05 +0100

    index

---
+++ style/script.js
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------------*/
+/*	00. navbar che scende
+/*-----------------------------------------------------------------------------------*/
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  } 
+
+}
