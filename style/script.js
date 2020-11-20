0f250737062325b53453c927f3ebd0406545f522
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2020-11-20 14:11:14 +0100

    menu down

--- style/script.js
+++ style/script.js
@@ -7,9 +7,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbardown").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
-  } 
+    document.getElementById("navbardown").style.top = "-70px";
+  }
 
 }
